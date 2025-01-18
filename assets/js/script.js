b64f998b52900c74eaa9d4dc7e64b3db698238c8
Katherin Meza G <katherin_meza@yahoo.com>
2025-01-17 23:01:55 -0300

    grafico incorporado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+let chartInstance;
+
+
 async function convertirMoneda() {
 const monto = document.getElementById("monto").value;
 const moneda = document.getElementById("moneda").value;
@@ -18,7 +21,42 @@ valorCambio = data.dolar.valor;
 const conversion = ( monto / valorCambio );
 resultado.innerText = `${conversion} ${moneda}
 ` 
+renderizarGrafico(moneda)
 } catch (error) {
     console.log(error);
 }
+}
+
+async function renderizarGrafico(moneda){
+    try {
+        const response = await fetch (`https://mindicador.cl/api/${moneda}`)
+        const data = await response.json();
+       console.log("data grafico: ",JSON.stringify(data,null,4));
+
+
+        const datosInvertidos = data.serie.slice(0,10).reverse()
+        const labels = datosInvertidos.map(item => item.fecha.slice(0,10));
+        const valores = datosInvertidos.map(item => item.valor);
+        const ctx = document.getElementById("grafico").getContext("2d");
+
+        chartInstance = new Chart( ctx,{
+            type: "line",
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: `Ultimos 10 dias(${moneda})`,
+                    data: valores,
+                    borderColor: "rgb(216, 28, 28)"
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+            }
+        }
+
+        )
+    } catch (error) {
+        
+    }
 }
